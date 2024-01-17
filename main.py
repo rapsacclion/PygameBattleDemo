134bd795a3eb7d3046cf4eeba501761134612367
Rapsacclion <44624376+rapsacclion@users.noreply.github.com>
2024-01-16 17:28:21 -0800

    Collision management fix and boss fight setup
    Additionally, added better texture loading.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ pygame.display.set_caption('demo')
 TICK = pygame.USEREVENT + 1
 pygame.time.set_timer(TICK, 250)
 paragraph = ""
+glitchness = 0
 black = (0,0,0)
 white = (255,255,255)
 clock = pygame.time.Clock()
@@ -18,7 +19,10 @@ crashed = False
 onTick = False
 x_pos = 0
 y_pos = 0
+xfpos = 500
+yfpos = 600
 fightbound = ((300, 300), (450, 450))
+
 
 # image imports
 alphabet = []
@@ -33,12 +37,20 @@ for t in range(0, 30):
     alphabet.append(pygame.image.load("alphabet"+iternum+".png"))
 for t in range(0, 5):
     playerbody.append(pygame.image.load("character_"+str(t)+".png"))
-bgimg = pygame.image.load("fight_bg0.png")
+playerrect = playerbody[0].get_rect()
+bgimg = pygame.image.load("fight_bg0.png").convert_alpha()
 bgimg = pygame.transform.scale(bgimg, (333, 166))
+bgrect = bgimg.get_rect()
+bgrect.center = (500, 600)
+bgmask = pygame.mask.from_surface(bgimg)
 title = pygame.image.load("title_1.png")
-skippitydoodah = pygame.image.load("title_2.png")
 title = pygame.transform.scale(title, (1000, 1000))
+skippitydoodah = pygame.image.load("title_2.png")
 skippitydoodah = pygame.transform.scale(skippitydoodah, (1000, 1000))
+fight_1 = pygame.image.load("fight00.png")
+fightrect = fight_1.get_rect()
+fightmask = pygame.mask.from_surface(fight_1)
+
 spek = pygame.mixer.Sound("charspeak1.wav")
 
 #function declaration
@@ -51,20 +63,44 @@ def printat(x, y, letter):
         pass
 def character(direction):
     gameDisplay.blit(playerbody[direction], (display_width/2-32, display_height/2-32))
-    
+def getcol(rect1, rect2, mask1, mask2):
+    #print(rect1.topright, rect2.topright)
+    offset = (rect1.topright[0]-rect2.topright[0], rect1.topright[1]-rect2.topright[1]+rect1.height-rect2.height)
+    yes = mask1.overlap(mask2, offset)
+    return yes
+
 #final setup
 pygame.mixer.music.set_volume(0.7)
 gameDisplay.fill(white)
+go = False
 direction = 0
 mode = 0
 #mode = 0 - title
 #mode = 1 - fight
+#mode = 2 - play
 debug = True
 aaaaaa = 4
-
+bossphase = 0
+'''
+0 = cutscene
+1 = simple
+2 = fire hands
+3 = claws circle fire hands
+4 = eyes circle claws hands fire hands hands
+5 = everything
+'''
+bosstalk = {
+    0: "number zero",
+    1: "test one",
+    2: "test the two",
+    3: "there is a three",
+    4: "two and two",
+    5: "i n f i n i  t e   p o w e r"
+}
 #main loop
 while not crashed:
     gameDisplay.fill(white)
+    playerrect.center = (x_pos, y_pos)
     if mode == 0:
         if aaaaaa >= 2:
             gameDisplay.blit(skippitydoodah, (0, 0))
@@ -73,30 +109,54 @@ while not crashed:
             gameDisplay.blit(title, (0, 0))
     pressed_keys = pygame.key.get_pressed()
     if mode == 1:
-        if y_pos > fightbound[1][1]-fightbound[0][1]:
-            y_pos -= 5
-        if x_pos < fightbound[1][0]-fightbound[0][0]:
-            x_pos += 5
-        if y_pos < fightbound[1][1]-fightbound[0][1]:
-            y_pos += 5
-        if x_pos > fightbound[1][0]-fightbound[0][0]:
-            x_pos -= 5
+        gameDisplay.blit(bgimg, bgrect)
+        fightrect.center = (xfpos, yfpos)
+        gameDisplay.blit(fight_1, fightrect)
+        if not getcol(bgrect, fightrect, bgmask, fightmask):
+            if xfpos > bgrect.center[0]:
+                xfpos -= 5
+            if xfpos < bgrect.center[0]:
+                xfpos += 5
+            if yfpos > bgrect.center[1]:
+                yfpos -= 5
+            if yfpos < bgrect.center[1]:
+                yfpos += 5
+        if bossphase != 6:
+            paragraph = bosstalk[bossphase]
+            if go:
+                bossphase += 1
+        else:
+            paragraph = 'congrats'
+        if bossphase == 0:
+            pass
+        if pressed_keys[pygame.K_w]:
+            yfpos -= 5
+        if pressed_keys[pygame.K_a]:
+            xfpos -= 5
+        if pressed_keys[pygame.K_s]:
+            yfpos += 5
+        if pressed_keys[pygame.K_d]:
+            xfpos += 5
+    if mode == 2:
         if pressed_keys[pygame.K_w]:
             direction = 3
             y_pos += 5
-            paragraph = "e!"
+            #paragraph = "e!"
         if pressed_keys[pygame.K_a]:
             direction = 2
             x_pos += 5
-            paragraph = "ah!"
+            #paragraph = "ah!"
         if pressed_keys[pygame.K_s]:
             direction = 0
             y_pos -= 5
-            paragraph = "ooh!"
+            #paragraph = "ooh!"
         if pressed_keys[pygame.K_d]:
             direction = 1
             x_pos -= 5
-            paragraph = "skippity boop dobbity dah dippity dee dop ba doo"
+            #paragraph = "skippity boop dobbity dah dippity dee dop ba doo"
+        gameDisplay.blit(bgimg, (x_pos, y_pos))
+        character(direction)
+    go = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
@@ -110,27 +170,17 @@ while not crashed:
             if event.key == pygame.K_SPACE:
                 if mode == 0:
                     mode = 1
+                else:
+                    go = True
             if event.key == pygame.K_RETURN:
                 print(x_pos,y_pos)
-    if mode == 1:
-        gameDisplay.blit(bgimg, (x_pos+fightbound[0][0], y_pos+fightbound[0][1]))
-        character(direction)
-        '''
-    if mode == 2:
-        gameDisplay.blit(bgimg, (x_pos, y_pos))
-        character(direction)
-    '''
-    if debug and mode == 1:
-        pygame.draw.rect(gameDisplay, (0, 0, 0, 15), (fightbound[0][0]+x_pos, fightbound[0][1]+y_pos, fightbound[1][0]-fightbound[0][0], fightbound[1][1]-fightbound[0][1]))
-    
     if paragraph != "": #for text
         x = 0
         y = 0
-        glitchness = 0
         pygame.draw.rect(gameDisplay, black, (48, 784, 864, 96))
         pygame.draw.rect(gameDisplay, white, (64, 800, 832, 64))
         for letter in paragraph:
-            printat(x+64, y+800+random.randint(-glitchness, glitchness), letter)
+            printat(x+64+(random.randint(-glitchness, glitchness)/2), y+800+random.randint(-glitchness, glitchness), letter)
             x += 32
             if x >= 832:
                 x = 0
